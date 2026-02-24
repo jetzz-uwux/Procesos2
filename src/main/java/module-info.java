dc6dc72015f80aaaaeed75c9f55b8adfa5e6867c
CamPr0ngs <camilacrespozurita@gmail.com>
2026-02-23 22:10:47 -0600

    Añade modulos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module progs.proyectobibliotecafx {
     requires javafx.controls;
     requires javafx.fxml;
+    
+    requires java.sql;     
+    requires java.desktop;  
+    requires jbcrypt;       
 
     opens progs.proyectobibliotecafx.controller to javafx.fxml;
     exports progs.proyectobibliotecafx;
